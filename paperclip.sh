2b0cf262068868a42a2bc452fd2e8ccf4511ae87
Eric <ehock7229@outlook.com>
2020-07-12 17:35:07 -0400

    add setMaxPlayers method to Bukkit

--- paperclip.sh
+++ paperclip.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-cp ./PaperSpigot-Server/target/paperspigot*-SNAPSHOT.jar ./Paperclip/paperspigot-1.8.8.jar
+cp ./PotionSpigot-Server/target/potionspigot*-SNAPSHOT.jar ./Paperclip/potionspigot-1.8.8.jar
 cp ./work/1.8.8/1.8.8.jar ./Paperclip/minecraft_server.1.8.8.jar
 cd ./Paperclip
 mvn clean package
 cd ..
-cp ./Paperclip/target/paperclip*-SNAPSHOT.jar ./Paperclip.jar
+cp ./Paperclip/target/potionspigot*-SNAPSHOT.jar ./PotionSpigot.jar
 
 echo ""
 echo ""
